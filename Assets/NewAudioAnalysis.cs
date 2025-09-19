fd940885fac1be455b11017dd1808c1ce28b3a01
JosephSu23 <meizhou.su@mail.utoronto.ca>
2025-09-19 14:16:33 -0400

    Implemented loudness detection for Microphone input
    Changed NewAudioAnalysis.cs so that it can detect the default voice input device and analyze loudness of microphone voice input. Added a test cube in the scene that would scale as loudness from microphone input changes. To test, simply run the scene and the mic will start receiving input.

--- Assets/NewAudioAnalysis.cs
+++ Assets/NewAudioAnalysis.cs
@@ -1,19 +1,32 @@
+using TMPro;
 using UnityEngine;
 
 public class NewAudioAnalysis : MonoBehaviour
 {
     public int sampleWindow = 512;//original value 64 -- must be power of 2
-    
+    private AudioClip microphoneClip;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        MicrophoneToAudioClip();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void MicrophoneToAudioClip()
+    {
+        string microphoneName = Microphone.devices[0];
+        microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
+    }
+
+    public float GetLoudnessFromMicrophone() 
+    {
+        return GetLoudnessFromAudioClip(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
     }
 
     public float GetLoudnessFromAudioClip(int clipPosition, AudioClip clip)
