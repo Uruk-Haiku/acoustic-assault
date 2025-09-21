ddac5b251e2aa31c8e8b8ca8bd4a228a2179e340
Uruk-Haiku <bpanet@gmail.com>
2025-09-21 16:56:05 -0400

    First commit - setting up game state

---
+++ Assets/GameState.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    // Variable declarations
+    [Header("Game State Variables")]
+    public int masterState = 0; // 0 = title screen, 1 = level selection, 2 = results screen, 10 = "I Want It That Way",
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
