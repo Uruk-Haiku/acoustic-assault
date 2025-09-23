1f2cec5a46eb3d3782e424414a94841b6060d213
Ben Panet <59447359+Uruk-Haiku@users.noreply.github.com>
2025-09-22 21:42:56 -0400

    Setting up title UI and scene

--- Assets/GameState.cs
+++ Assets/GameState.cs
@@ -4,17 +4,27 @@ public class GameState : MonoBehaviour
 {
     // Variable declarations
     [Header("Game State Variables")]
-    public int masterState = 0; // 0 = title screen, 1 = level selection, 2 = results screen, 10 = "I Want It That Way",
+    public int masterState = 0; // 0 = title screen, 1 = level selection, 2 = results screen, 10 = "I Want It That Way"
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        masterState = 0; // Initialize to title screen
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SetMasterState(int state)
+    {
+        masterState = state;
+    }
+
+    public int getMasterState()
+    {
+        return masterState;
     }
 }
